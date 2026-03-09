07ffec8a0d5cac89f78498b054ba3edd3f3112b9
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-03-08 21:14:13 -0400

    ajout structure classe Voiture

---
+++ atelier3-parc-voitures.py
@@ -0,0 +1,9 @@
+class Voiture:
+    def __init__(self, marque, modele, annee, kilometrage):
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.kilometrage = kilometrage
+
+    def afficher_info(self):
+        pass
